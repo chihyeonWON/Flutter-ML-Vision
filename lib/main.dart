bb50a5ddf716ef0380cec8e4f1309b0064d7de6e
ylose <heungeob1003@gmail.com>
2023-01-19 16:07:03 +0900

    예제코드 추가

--- lib/main.dart
+++ lib/main.dart
@@ -23,8 +23,41 @@ class MyApp extends StatelessWidget {
 
 class firstPage extends StatelessWidget {
 
+  PickedFile? _image;
+
+  Future getImageFromCam() async {
+    var image =
+    await ImagePicker.platform.pickImage(source: ImageSource.camera);
+    _image = image;
+    print(_image!.path);
+  }
+
+  Future getImageFromGallery() async {
+    var image =
+    await ImagePicker.platform.pickImage(source: ImageSource.gallery);
+    _image = image;
+    print(_image!.path);
+  }
+
+  late GoogleVisionImage visionImage;
+  TextRecognizer textRecognizer = GoogleVision.instance.textRecognizer();
+  late VisionText visionText;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body:Padding(
+        padding: const EdgeInsets.only(top: 16.0),
+        child: FloatingActionButton(
+          backgroundColor: Colors.red,
+          onPressed: () {
+            getImageFromCam();
+          },
+          heroTag: 'video0',
+          tooltip: 'Pick Video from gallery',
+          child: const Icon(Icons.video_library),
+        ),
+      ),
+    );
   }
 }
